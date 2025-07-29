26e3aaa6e8432dba8ba41535f173ad8a01f8eaf2
harshit7045 <harshit7045@outloook.com>
2025-07-30 00:50:25 +0530

    {commit_message}

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Frontend development", "Backend development", "Software development", "Internet of Things", "web development"],
+    strings: ["Frontend development", "Backend development", "Software development", "Internet of Things", "Web development","Inventor"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
